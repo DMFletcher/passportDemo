261cf51be2317ae064bd630712f0f4eb69c7786d
DMFletcher <thanatos.02@gmail.com>
2018-05-17 17:20:59 -0500

    working

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -8,3 +8,18 @@ exports.signin = function(req, res) {
     res.render('signin');
  
 }
+exports.dashboard = function(req, res) {
+ 
+    res.render('dashboard');
+ 
+}
+
+exports.logout = function(req, res) {
+ 
+    req.session.destroy(function(err) {
+ 
+        res.redirect('/');
+ 
+    });
+ 
+}
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,6 @@
 var authController = require('../controllers/authcontroller.js');
 
-module.exports = function (app) {
+module.exports = function (app, passport) {
 
     app.get('/signup', authController.signup);
 
@@ -13,6 +13,28 @@ module.exports = function (app) {
     }
 
     ));
+
+    app.get('/dashboard', isLoggedIn, authController.dashboard);
+
+    app.get('/logout', authController.logout);
+
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/dashboard',
+ 
+        failureRedirect: '/signin'
+    }
+ 
+));
+    function isLoggedIn(req, res, next) {
+
+        if (req.isAuthenticated())
+
+            return next();
+
+        res.redirect('/signin');
+
+    }
+    
 }
 
 
